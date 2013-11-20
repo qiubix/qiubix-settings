f9284611e291f8cac95d718a454dc23a9f32f9a3
qiubix <qiubix@gmail.com>
2013-11-20 11:37:36 +0100

    added gitsetup.sh

---
+++ gitsetup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#installing and setting up git
+
+sudo apt-get update
+sudo apt-get install git
+sudo apt-get install git-core
+sudo apt-get install xclip
+
+git config --global user.name qiubix
+git config --global user.email qiubix@gmail.com
+git config --global github.user qiubix
+git config --global push.default matching
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
+git config --global color.ui true
+git config --global branch.autosetuprebase always
+
+ssh-keygen -t rsa -C "qiubix@gmail.com"
+cd ~/.ssh
+ssh-add id_rsa
+xclip -sel clip < ~/.ssh/id_rsa.pub
