e584da2b7561a30731233fefac7cff1a658419d2
qiubix <qiubix@gmail.com>
2013-11-20 11:37:16 +0100

    added extras.sh

---
+++ extras.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+clear
+
+sudo add-apt-repository ppa:cairo-dock-team/ppa
+
+sudo apt-get update
+
+sudo apt-get install gconf-editor
+sudo apt-get install cairo-dock
+sudo apt-get install cairo-dock-plugins
+sudo apt-get install flashplugin-installer
+sudo apt-get install ubuntu-restricted-extras
+sudo apt-get install compizconfig-settings-manager
+sudo apt-get install conky curl lm-sensors hddtemp
+
