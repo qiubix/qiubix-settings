775126a377b1fe952a5fe3c8f9cd4ce684d65025
qiubix <qiubix@gmail.com>
2014-06-04 09:28:07 +0000

    Create vimconfig.sh for all plugins installation

---
+++ scripts/vimconfig.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+clear
+
+# ========== install vim ==========
+sudo apt-get install vim
+
+# ========== copy vim settings from settings repo ==========
+cp ~/qiubix-settings/.vimrc ~
+
+# ========== install pathogen ==========
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# ==========================================
+# ========== PLUGINS INSTALLATION ==========
+# ==========================================
+
+# ========== airline ==========
+git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
+
+# ========== git-gutter ==========
+git clone git://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
+
+# ========== fugitive ==========
+git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
