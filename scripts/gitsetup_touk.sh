ac60d4a938426ab0a3727df8474a1df0ef44c439
Karol Katerzawa <kkt@touk.pl>
2014-10-08 09:13:35 +0200

    Add git setup for another account

---
+++ scripts/gitsetup_touk.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#installing and setting up git
+
+sudo apt-get update
+sudo apt-get install git git-core xclip gitk gitg
+
+git config --global user.name "Karol Katerzawa"
+git config --global user.email kkt@touk.pl
+git config --global push.default matching
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
+git config --global color.ui true
+git config --global branch.autosetuprebase always
+git config --global merge.tool kdiff3
+git config --global core.editor vim
+
+ssh-keygen -t rsa -C "kkt@touk.pl"
+cd ~/.ssh
+ssh-add id_rsa
+xclip -sel clip < ~/.ssh/id_rsa.pub
+
+
+echo '===============================================
+SSH Key generated and copied to clipboard.
+Add it in your account settings on github and bitbucket.'
