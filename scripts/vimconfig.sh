78ba5abbd6c7171e7611e1a94094d20003744856
qiubix <qiubix@gmail.com>
2014-10-20 11:08:04 +0200

    Add directory check before plugin installation

--- scripts/vimconfig.sh
+++ scripts/vimconfig.sh
@@ -21,16 +21,31 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 echo "Installing plugins..."
 
 # ========== airline ==========
-git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
+if [ ! -d ~/.vim/bundle/vim-airline ]; then
+  git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
+fi
+
+# ========== surround ==========
+if [ ! -d ~/.vim/bundle/vim-airline ]; then
+  git clone https://github.com/tpope/vim-surround.git ~/.vim/bundle/vim-surround
+fi
 
 # ========== git-gutter ==========
-git clone git://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
+if [ ! -d ~/.vim/bundle/vim-gitgutter ]; then
+  git clone git://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
+fi
 
 # ========== fugitive ==========
-git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+if [ ! -d ~/.vim/bundle/vim-fugitive ]; then
+  git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+fi
 
 # ========== numbers ==========
-git clone https://github.com/myusuf3/numbers.vim.git ~/.vim/bundle/numbers
+if [ ! -d ~/.vim/bundle/numbers ]; then
+  git clone https://github.com/myusuf3/numbers.vim.git ~/.vim/bundle/numbers
+fi
 
 # ========== solarized color theme ==========
-git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+if [ ! -d ~/.vim/bundle/vim-colors-solarized ]; then
+  git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+fi
