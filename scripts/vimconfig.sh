04eb3de237ee3e1df78ee9ca6e451b9912e4fdc5
qiubix <qiubix@gmail.com>
2014-12-04 10:30:01 +0100

    Rearrange plugins, install vinegar plugin

--- scripts/vimconfig.sh
+++ scripts/vimconfig.sh
@@ -27,20 +27,16 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 # ========== PLUGINS INSTALLATION ==========
 # ==========================================
 echo "Installing plugins..."
+# ...from most to least useful
+
+# ========== CtrlP ==========
+if [ ! -d ~/.vim/bundle/ctrlp ]; then
+  git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp
+fi
 
 # ========== airline ==========
 if [ ! -d ~/.vim/bundle/vim-airline ]; then
   git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
-fi
-
-# ========== surround ==========
-if [ ! -d ~/.vim/bundle/vim-surround ]; then
-  git clone https://github.com/tpope/vim-surround.git ~/.vim/bundle/vim-surround
-fi
-
-# ========== git-gutter ==========
-if [ ! -d ~/.vim/bundle/vim-gitgutter ]; then
-  git clone git://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
 fi
 
 # ========== fugitive ==========
@@ -48,9 +44,14 @@ if [ ! -d ~/.vim/bundle/vim-fugitive ]; then
   git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
 fi
 
-# ========== CtrlP ==========
-if [ ! -d ~/.vim/bundle/ctrlp ]; then
-  git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp
+# ========== git-gutter ==========
+if [ ! -d ~/.vim/bundle/vim-gitgutter ]; then
+  git clone git://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
+fi
+
+# ========== solarized color theme ==========
+if [ ! -d ~/.vim/bundle/vim-colors-solarized ]; then
+  git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
 fi
 
 # ========== NERDTree ==========
@@ -58,12 +59,17 @@ if [ ! -d ~/.vim/bundle/nerdtree ]; then
   git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 fi
 
+# ========== vinegar ==========
+if [ ! -d ~/.vim/bundle/vim-vinegar ]; then
+  git clone https://github.com/tpope/vim-vinegar ~/.vim/bundle/vim-vinegar
+fi
+
 # ========== numbers ==========
 if [ ! -d ~/.vim/bundle/numbers ]; then
   git clone https://github.com/myusuf3/numbers.vim.git ~/.vim/bundle/numbers
 fi
 
-# ========== solarized color theme ==========
-if [ ! -d ~/.vim/bundle/vim-colors-solarized ]; then
-  git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+# ========== surround ==========
+if [ ! -d ~/.vim/bundle/vim-surround ]; then
+  git clone https://github.com/tpope/vim-surround.git ~/.vim/bundle/vim-surround
 fi
