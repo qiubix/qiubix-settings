6bc1faf0c576e9ec6b60e057bed85fef053832be
qiubix <qiubix@gmail.com>
2015-01-31 18:54:17 +0100

    Update MacOs theme for Ubuntu 14.04

--- scripts/themes.sh
+++ scripts/themes.sh
@@ -14,15 +14,8 @@ sudo apt-get update
 sudo apt-get install faenza-icon-theme dconf-cli
 
 #install mac os lion theme and icons
-sudo apt-get install mac-os-lion-icons-v2
-sudo apt-get install mac-os-lion-theme-v2
-
-#install mac cursors
-wget -O mac-cursors.zip http://goo.gl/eYtho
-sudo unzip mac-cursors.zip -d /usr/share/icons/; rm mac-cursors.zip
-cd /usr/share/icons/mac-cursors
-sudo chmod +x install-mac-cursors.sh uninstall-mac-cursors.sh
-sudo ./install-mac-cursors.sh
+sudo apt-get install mac-icons-v3
+sudo apt-get install mac-ithemes-v3
 
 #Mac spotlight-like search, synapse
 sudo apt-get install indicator-synapse
