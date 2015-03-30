e561117fc039cb2245501553357f19e4076f3a28
Karol Katerzawa <qiubix@gmail.com>
2015-03-30 15:39:25 +0200

    Fix discode-setup script
    Update dependencies
    install CvCoreTypes before CvBasic

--- scripts/discode-setup.sh
+++ scripts/discode-setup.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-clear
-
-sudo add-apt-repository ppa:pyside
 sudo add-apt-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl
 
 sudo apt-get update
 
 # ====== dependencies installation ======
 echo "Installing dependencies..."
-sudo apt-get install cmake cmake-curses-gui libqt4-dev libboost-dev libopencv-dev python3.2 python3-pyside libpcl-all
+sudo apt-get install cmake cmake-curses-gui libqt4-dev libboost-dev libboost-program-options-dev libboost-filesystem-dev libboost-program-options-dev libboost-iostreams-dev libboost-regex-dev libboost-regex-dev libboost-serialization-dev libboost-system-dev libboost-thread-dev libboost-timer-dev libopencv-dev python3 python3-pyside libpcl-all
 
 # ====== DisCODe installation ======
 echo "Installing DisCODe..."
@@ -21,12 +18,12 @@ cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/bin/DisCODe && make && make install
 echo "Setting up basic DCL..."
 mkdir -p $HOME/src/DCL
 
+# CvCoreTypes
+git clone https://github.com/DisCODe/CvCoreTypes $HOME/src/DCL/CvCoreTypes
+mkdir -p $HOME/src/DCL/CvCoreTypes/build && cd $HOME/src/DCL/CvCoreTypes/build
+cmake .. && make -j4 && make install
+
 # CvBasic
 git clone https://github.com/DisCODe/DCL_CvBasic.git $HOME/src/DCL/CvBasic
 mkdir -p $HOME/src/DCL/CvBasic/build && cd $HOME/src/DCL/CvBasic/build
-cmake .. && make -j4 && make install
-
-# CvCoreTypes
-git clone https://github.com/DisCODe/DCL_CvCoreTypes $HOME/src/DCL/CvCoreTypes
-mkdir -p $HOME/src/DCL/CvCoreTypes/build && cd $HOME/src/DCL/CvCoreTypes/build
 cmake .. && make -j4 && make install
