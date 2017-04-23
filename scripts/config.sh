8a8e09b36ce876652fc8cd0e995b32acd2b47e02
qiubix <qiubix@gmail.com>
2017-04-23 17:54:28 +0200

    Remap CAPS LOCK key to CTRL in config script

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-clear
 echo "Configuring shell..."
 
+echo "Setting ignore case on completion..."
 echo "set completion-ignore-case On" >> ~/.inputrc
+echo "Done."
+
+echo "Mapping CAPS LOCK key to LEFT CTRL..."
+echo "XKBOPTIONS=\"ctrl:nocaps\"" | sudo tee -a /etc/default/keyboard
+echo "Done. Logout and login for changes to impact your system."
