db79f01a3d588271bdb7402af1212c4d1316ca38
qiubix <qiubix@gmail.com>
2018-01-21 00:46:05 +0100

    Add config for Java coding tools

---
+++ scripts/coding-tools-java.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Configuring java development environment..."
+
+echo "Installing Oracle JDK 8..."
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt update
+sudo apt install -y oracle-java8-installer
+echo "JAVA_HOME=/usr/lib/jvm/java-8-oracle" | sudo tee -a /etc/environment
+
+echo "Installing JVM tools..."
+sudo apt-get install maven gradle
