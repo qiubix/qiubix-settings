604b783f459bc4003560b7381039b2d88f5e116a
qiubix <qiubix@gmail.com>
2023-06-30 16:23:37 +0200

    Add global bootstrap script

---
+++ bootstrap-mac.sh
@@ -0,0 +1,33 @@
+#!/bin/zsh
+
+echo "Setting up brew..."
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew tap homebrew/cask-versions
+
+echo "Setting up git..."
+brew install git
+
+echo "Setting up zsh..."
+brew install tmux
+
+echo "Setting up vim..."
+brew install vim
+
+echo "Installing useful tools..."
+brew install tree
+
+echo "Setting up JVM dev env..."
+brew install --cash corretto17 corretto11
+brew install --cask jetbrains-toolbox
+git clone https://github.com/jenv/jenv.git $HOME/.jenv
+brew install maven
+
+echo "Setting up Python env..."
+brew install pyenv python3
+
+echo "Installing apps..."
+brew install --cask spotify
+brew install --cask timeular
+brew install --cask obsidian
+brew install --cask todoist
+brew install --cask vanilla rocket horo
