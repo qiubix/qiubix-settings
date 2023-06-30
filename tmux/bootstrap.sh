19cb9a94258e276a742f110fc63cc34555718ac7
qiubix <qiubix@gmail.com>
2023-06-30 15:51:22 +0200

    tmux: Init bootstrap

---
+++ tmux/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+brew install tmux tmuxp
+
+export TERM=xterm-256color
+# alias tmux='tmux -2'
+# alias tmux='TERM=xterm-256color /home/$USER/bin/tmux -2'
+# alias start-tmux='~/tmux-startup.sh && tmux attach -t coding'
+
