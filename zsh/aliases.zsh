c79442348bf911927e561e6dfc3b4eecdeec2a99
qiubix <qiubix@gmail.com>
2023-06-30 15:55:57 +0200

    zsh: Enable git plugin back

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -19,6 +19,7 @@ alias tree='tree -CF'
 # additional git aliases
 # ----------------------
 alias gst='git status'
+alias gc='git commit --verbose'
 alias ga='git add'
 alias gdl='git ls-files'
 alias gd='git diff'
@@ -33,9 +34,15 @@ function current_branch() {
   ref=$(git rev-parse --short HEAD 2> /dev/null) || return
   echo ${ref#refs/heads/}
 }
-alias ggpull='git pull origin $(current_branch)'
-alias ggpur='git pull --rebase origin $(current_branch)'
-alias ggpush='git push origin $(current_branch)'
+
+function git_current_branch() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || \
+  ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  echo ${ref#refs/heads/}
+}
+# alias ggpull='git pull origin $(current_branch)'
+# alias ggpur='git pull --rebase origin $(current_branch)'
+# alias ggpush='git push origin $(current_branch)'
 
 # ----------------------
 # other aliases
