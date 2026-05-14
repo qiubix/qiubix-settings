a71c5e95d14407d903b7bd9778fde45b59ef472d
qiubix <karol.katerzawa@syncron.com>
2026-05-14 13:13:45 +0200

    Add project-based tmux configs

--- tmux/start-tmux.sh
+++ tmux/start-tmux.sh
@@ -3,8 +3,13 @@
 echo "Starting multiple tmux sessions"
 
 tmuxp load -d config
-tmuxp load -d pyspark-sandbox
-tmuxp load -d aws-deploy
-tmux new-session -s coding
+# tmuxp load -d pyspark-sandbox
+# tmuxp load -d aws-deploy
+tmuxp load -d xfor
+tmuxp load -d gim
+tmuxp load -d ds-generic-experiment
+tmuxp load -d side-hustle
+tmuxp load -d data-platform
+# tmux new-session -s coding
 
 tmux ls
