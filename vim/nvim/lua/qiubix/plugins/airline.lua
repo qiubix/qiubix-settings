f7a7d782e8b2c5b9eb38bdf2bb80d88a54bf5700
qiubix <karol.katerzawa@syncron.com>
2026-09-17 11:40:20 +0200

    vim: Install render-markdown

--- vim/nvim/lua/qiubix/plugins/airline.lua
+++ vim/nvim/lua/qiubix/plugins/airline.lua
@@ -11,15 +11,15 @@ return {
     vim.g.airline_solarized_bg = 'dark'
     vim.g['airline#extensions#tabline#enabled'] = 1
     vim.g['airline#extensions#branch#enabled'] = 1
-    vim.g.airline_left_sep = ''
-    vim.g.airline_left_alt_sep = ''
-    vim.g.airline_right_sep = ''
-    vim.g.airline_right_alt_sep = ''
-    vim.g['airline_symbols.branch'] = ''
-    vim.g['airline_symbols.colnr'] = ' ℅:'
-    vim.g['airline_symbols.readonly'] = ''
-    vim.g['airline_symbols.linenr'] = ' :'
-    vim.g['airline_symbols.maxlinenr'] = '☰ '
+		vim.g.airline_left_sep = ''
+		vim.g.airline_left_alt_sep = ''
+		vim.g.airline_right_sep = ''
+		vim.g.airline_right_alt_sep = ''
+		vim.g['airline_symbols.branch'] = ''
+		vim.g['airline_symbols.colnr'] = ' ℅:'
+		vim.g['airline_symbols.readonly'] = ''
+		vim.g['airline_symbols.linenr'] = ' :'
+		vim.g['airline_symbols.maxlinenr'] = '☰ '
     vim.g['airline_symbols.dirty'] = '⚡'
   end,
 }
